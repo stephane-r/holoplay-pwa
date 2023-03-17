30aae84224ffacce92d5e78a6a1509f622c6d822
Stéphane <s.richin@oodrive.com>
2023-03-17 15:38:16 +0100

    feat: show correct logo between colorscheme

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,21 @@
+import { ColorScheme } from "@mantine/core";
+import { useLocalStorage } from "@mantine/hooks";
 import { memo } from "react";
 import { Link } from "react-router-dom";
 
 export const Logo = memo(() => {
+  const [colorScheme] = useLocalStorage<ColorScheme>({
+    key: "mantine-color-scheme",
+  });
+
+  const isDarkMode = colorScheme === "dark";
+  const imageSrc = isDarkMode
+    ? "/logo-holoplay-white.png"
+    : "/logo-holoplay-blue.png";
+
   return (
     <Link to="/" style={{ marginTop: 8 }}>
-      <img src="/logo-holoplay-white.png" width={40} alt="Logo HoloPlay" />
+      <img src={imageSrc} width={40} alt="Logo HoloPlay" />
     </Link>
   );
 });
