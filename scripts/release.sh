baf69c7dca98a69b91374154ff9f77bdfdb961fd
Stéphane <s.richin@oodrive.com>
2023-07-13 11:35:14 +0200

    chore: update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
 set -e
 
-# Get package.json version
-PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
-
-# Commit files
-git add .
-git commit -m "chore: release v$PACKAGE_VERSION"
-
-# Create new git tag
-git tag "v$PACKAGE_VERSION"
-git tag -l
-
 # Push to GitHub
 git push origin develop
 git push origin --tags
