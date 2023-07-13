0d74babaf6065a303b9b8cdf9fd3c3dd0487dcfe
Stéphane <s.richin@oodrive.com>
2023-07-13 11:26:39 +0200

    chore: update github worflows

---
+++ scripts/release.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+# Get package.json version
+PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
+
+# Commit files
+git add .
+git commit -m "chore: release v$PACKAGE_VERSION"
+
+# Create new git tag
+git tag "v$PACKAGE_VERSION"
+git tag -l
+
+# Push to GitHub
+git push origin develop
+git push origin "v$PACKAGE_VERSION"
