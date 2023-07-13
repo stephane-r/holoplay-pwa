2d9f0fdc0260ffd2c92e6c74f804514282caa528
Stéphane <s.richin@oodrive.com>
2023-07-13 11:41:07 +0200

    chore: update github worflows

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -e
 
+# Checkout main and merge develop
+git checkout main
+git merge develop
+
+# Back to develop
+git checkout -
+
 # Push to GitHub
-git push origin develop
 git push origin --tags
+git push origin develop
+git push origin main
