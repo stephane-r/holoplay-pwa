d2d884bf178e04906ce2e2a7beca4546986db963
Stéphane <s.richin@oodrive.com>
2023-08-29 16:42:07 +0200

    feat: add generated genre list on dashboard

---
+++ src/components/Genre.tsx
@@ -0,0 +1,84 @@
+import {
+  Box,
+  Flex,
+  Space,
+  Text,
+  Title,
+  UnstyledButton,
+  createStyles,
+} from "@mantine/core";
+import { memo } from "react";
+import { genres } from "../utils/genres";
+import { useHover } from "@mantine/hooks";
+import { useSearchValues, useSetSearchValues } from "../providers/Search";
+
+const useStyles = createStyles((theme) => ({
+  item: {
+    width: 90,
+    height: 90,
+    backgroundColor:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[6]
+        : theme.colors.gray[0],
+    borderRadius: 10,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    padding: theme.spacing.xs,
+    transition: "background-color 0.2s",
+  },
+  text: {
+    fontSize: theme.fontSizes.sm,
+  },
+}));
+
+export const GenreList = memo(() => {
+  return (
+    <>
+      <Title order={2}>Moods & genres</Title>
+      <Space h="lg" />
+      <Flex gap={20} wrap="wrap">
+        {genres.map((genre) => (
+          <Genre genre={genre} />
+        ))}
+      </Flex>
+    </>
+  );
+});
+
+const Genre = memo(
+  ({
+    genre,
+  }: {
+    genre: {
+      name: string;
+      color: string;
+      colorHover: string;
+    };
+  }) => {
+    const { classes } = useStyles();
+    const { hovered, ref } = useHover();
+    const setSearchValues = useSetSearchValues();
+    const searchValues = useSearchValues();
+
+    const handleClick = () => {
+      setSearchValues({ ...searchValues, q: genre.name });
+    };
+
+    return (
+      <UnstyledButton onClick={handleClick}>
+        <Box
+          ref={ref}
+          className={classes.item}
+          style={{
+            border: `solid 4px ${genre.color}`,
+            backgroundColor: hovered ? genre.color : undefined,
+          }}
+        >
+          <Text className={classes.text}>{genre.name}</Text>
+        </Box>
+      </UnstyledButton>
+    );
+  }
+);
