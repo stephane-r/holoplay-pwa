b18df0e6ca54060bcd73f8fe92e751c738ca2f35
Stéphane <s.richin@oodrive.com>
2023-11-02 11:43:46 +0100

    feat(settings): user can disabled plausible analytics

---
+++ src/components/Script.tsx
@@ -0,0 +1,12 @@
+import { memo, useMemo } from "react";
+import { useScript } from "../hooks/useScript";
+import { useSettings } from "../providers/Settings";
+
+const PLAUSIBLE_INSTANCE_SCRIPT_URL = "https://plausible.holoplay.io/js/script.js";
+
+export const Scripts = memo(() => {
+  const settings = useSettings();
+  const analyticsEnabled = useMemo(() => process.env.REACT_APP_PLAUSIBLE_ANALYTICS === "true" && settings.analytics, [settings.analytics]);
+  useScript(analyticsEnabled ? PLAUSIBLE_INSTANCE_SCRIPT_URL : null);
+  return null
+});
